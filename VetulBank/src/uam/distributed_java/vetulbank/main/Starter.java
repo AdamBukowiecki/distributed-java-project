c69d433f0ab58fc15d63cf647dfa0b87fde33a67
AdamBukowiecki <adambukowiecki@wp.pl>
2015-01-22 16:26:47 +0100

    0000

---
+++ VetulBank/src/uam/distributed_java/vetulbank/main/Starter.java
@@ -0,0 +1,12 @@
+package uam.distributed_java.vetulbank.main;
+
+import org.springframework.boot.SpringApplication;
+
+public class Starter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Starter.class, args);
+
+	}
+
+}
