fce38205956193b6f4d6926522aec5872ce930a1
AdamBukowiecki <adambukowiecki@wp.pl>
2015-01-28 20:23:21 +0100

    Zmiana hasel

--- VetulBankServer/src/uam/distributed_java/vetulbank/main/actors/ActorManager.java
+++ VetulBankServer/src/uam/distributed_java/vetulbank/main/actors/ActorManager.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import uam.distributed_java.vetulbank.main.actors.messages.ActorMessage;
+import uam.distributed_java.vetulbank.main.actors.messages.ChangePasswordMessage;
 import uam.distributed_java.vetulbank.main.actors.messages.TransactionMessage;
 import uam.distributed_java.vetulbank.main.exceptions.NotEnoughHajsException;
 import uam.distributed_java.vetulbank.main.models.Account;
@@ -26,57 +27,68 @@ public class ActorManager extends UntypedActor {
 	public void onReceive(Object arg0) throws Exception {
 		
 		if(arg0 instanceof ActorMessage) {
-			
-			ActorMessage message = (ActorMessage) arg0;
-			
-			switch(message.getCode()) {
-			case DELETE_ACCOUNT_BY_ID: {
-				ActorMessage<Boolean> parameterizedMessage = (ActorMessage<Boolean>) message;
-				accountRepository.delete(parameterizedMessage.getId());
-				break;
-			}
-			case GET_ACCOUNTS_TRANSACTIONS: {
-				ActorMessage<List<Transaction>> parameterizedMessage = (ActorMessage<List<Transaction>>) message;
-				Iterable<Transaction> transactions = transactionRepository.findAll();
-				List<Transaction> result = new ArrayList<>();
-				for(Transaction transaction: transactions) {
-					if( transaction.getFromId().equals(parameterizedMessage.getId()) ||
-						transaction.getTargetId().equals(parameterizedMessage.getId()) )
-						result.add(transaction);
-				}
-				parameterizedMessage.setResult(result);
-				break;
-			}
-			case GET_ACCOUNT_BY_ID: {
-				ActorMessage<Account> parameterizedMessage = (ActorMessage<Account>) message;
-				parameterizedMessage.setResult(accountRepository.findOne(parameterizedMessage.getId()));
-				break;
-			}
-			
-			case GET_ACCOUNT_PASSWORD: {
-				ActorMessage<String> parameterizedMessage = (ActorMessage<String>) message;
-				parameterizedMessage.setResult(accountRepository.findOne(parameterizedMessage.getId()).getPassword());
-				break;
-			}
-			
-			case CREATE_ACCOUNT: {
-				ActorMessage<Boolean> parameterizedMessage = (ActorMessage<Boolean>) message;
-				accountRepository.save(new Account(parameterizedMessage.getId(), 1000, "", new ArrayList<Transaction>()));
-			}
-			
-			default:
-				break;
-			
-			}
-			
+			actorMessageReaction((ActorMessage) arg0);
 		}
 		
 		if(arg0 instanceof TransactionMessage) {
 			transactionReaction((TransactionMessage) arg0);
 		}
 		
+		if(arg0 instanceof ChangePasswordMessage) {
+			changePassowrdReaction((ChangePasswordMessage) arg0);
+		}
+		
 	}
 	
+	private void actorMessageReaction(ActorMessage message) {
+
+		switch(message.getCode()) {
+		case DELETE_ACCOUNT_BY_ID: {
+			ActorMessage<Boolean> parameterizedMessage = (ActorMessage<Boolean>) message;
+			accountRepository.delete(parameterizedMessage.getId());
+			break;
+		}
+		case GET_ACCOUNTS_TRANSACTIONS: {
+			ActorMessage<List<Transaction>> parameterizedMessage = (ActorMessage<List<Transaction>>) message;
+			Iterable<Transaction> transactions = transactionRepository.findAll();
+			List<Transaction> result = new ArrayList<>();
+			for(Transaction transaction: transactions) {
+				if( transaction.getFromId().equals(parameterizedMessage.getId()) ||
+					transaction.getTargetId().equals(parameterizedMessage.getId()) )
+					result.add(transaction);
+			}
+			parameterizedMessage.setResult(result);
+			break;
+		}
+		case GET_ACCOUNT_BY_ID: {
+			ActorMessage<Account> parameterizedMessage = (ActorMessage<Account>) message;
+			parameterizedMessage.setResult(accountRepository.findOne(parameterizedMessage.getId()));
+			break;
+		}
+		
+		case GET_ACCOUNT_PASSWORD: {
+			ActorMessage<String> parameterizedMessage = (ActorMessage<String>) message;
+			parameterizedMessage.setResult(accountRepository.findOne(parameterizedMessage.getId()).getPassword());
+			break;
+		}
+		
+		case CREATE_ACCOUNT: {
+			ActorMessage<Boolean> parameterizedMessage = (ActorMessage<Boolean>) message;
+			accountRepository.save(new Account(parameterizedMessage.getId(), 1000, "", new ArrayList<Transaction>()));
+		}
+		
+		default:
+			break;
+		
+		}
+		
+		
+	}
+
+	private void changePassowrdReaction(ChangePasswordMessage message) {
+		accountRepository.findOne(message.getId()).setPassword(message.getPassword());
+	}
+
 	private void transactionReaction(TransactionMessage message) {
 
 		Transaction transaction = message.getTransaction();
--- VetulBankServer/src/uam/distributed_java/vetulbank/main/controllers/AccountController.java
+++ VetulBankServer/src/uam/distributed_java/vetulbank/main/controllers/AccountController.java
@@ -20,11 +20,10 @@ import com.fasterxml.jackson.annotation.JsonView;
 import uam.distributed_java.vetulbank.main.Starter;
 import uam.distributed_java.vetulbank.main.actors.ActorManager;
 import uam.distributed_java.vetulbank.main.actors.messages.ActorMessage;
+import uam.distributed_java.vetulbank.main.actors.messages.ChangePasswordMessage;
 import uam.distributed_java.vetulbank.main.actors.messages.MessageCodes;
 import uam.distributed_java.vetulbank.main.models.Account;
 import uam.distributed_java.vetulbank.main.models.Transaction;
-import uam.distributed_java.vetulbank.main.repositories.AccountRepository;
-
 
 /**
  * 
@@ -35,6 +34,7 @@ import uam.distributed_java.vetulbank.main.repositories.AccountRepository;
  * \ DELETE accounts/{id} - usuniecie Account'a o podanym id
  * \ PUT accounts/{id} - stworzenie i pobranie Account'a o podanym id
  * \ GET accounts/{id}/password/{pass} - zwroci Boolean'a czy Account id ma haslo pass
+ * \ PUT accounts/{id}/password/{pass} - zmiena haslo Account'a z podanym id na nowe rowne pass
  * 
  * @author s383930
  *
@@ -78,6 +78,13 @@ public class AccountController {
 		ActorMessage<Account> message = new ActorMessage<>(MessageCodes.CREATE_ACCOUNT, id);
 		ref.tell(message, ActorRef.noSender());
 		return message.getResult();
+	}
+	
+	@RequestMapping(value = "/{id}/password/{pass}", method = RequestMethod.PUT)
+	public void setPassword(@PathVariable String id, @PathVariable String pass) {
+		ActorRef ref = Starter.getActorSystem().actorOf(Props.create(ActorManager.class), "manager");
+		ChangePasswordMessage message = new ChangePasswordMessage(id, pass);
+		ref.tell(message, ActorRef.noSender());
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
